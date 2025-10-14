b48d1abb1073ec3467c482524b38611dcbeb7c35
Zsolt Varga <vargazsolti@gmail.com>
2025-10-14 21:51:29 +0200

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,7 +19,7 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     @can('users.read') 
     <x-nav-link :href="route('admin.users.index')" :active="request()->routeIs('admin.users.*')">
-        {{ __('Users') }}j
+        {{ __('Users') }}
     </x-nav-link>
 @endcan
                 </div>
