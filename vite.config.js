681071ba2a3ce560670e21989b15cd926821e3ba
Zsolt Varga <vargazsolti@gmail.com>
2025-10-16 19:19:55 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+  server: {
+    host: 'app.test',
+    port: 5174,                     // külön port a Blade asseteknek
+    origin: 'http://app.test:5174', // így a Blade oldalak helyes URL-t kapnak
+    cors: true,
+    hmr: { host: 'app.test' },
+  },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+})
